9c9e3aa564cdf1f0e6ac8729db48e24beecd8121
Ehyiah <rei_eva@hotmail.com>
2018-03-22 18:27:58 +0100

    update

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// INITIALISATION PARALLAX
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+//INITIALISATION MENU
+$(".dropdown-trigger").dropdown();
+
+// INITIALISATION MENU RESPONSIVE
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
